07d93dfa81347cd48c80716ddf4a8d3272210199
Felipex_King <146594144+FelipeX2001@users.noreply.github.com>
2024-03-17 08:03:18 +0000

    cambios texto header

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>AIPROP</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://aiprop.com.co/',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://api.whatsapp.com/send?phone=573042536416&text=Quiero%20evolucionar%20mi%20empresa%20con%20Inteligencia%20Artificial%20%F0%9F%A6%BE',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
